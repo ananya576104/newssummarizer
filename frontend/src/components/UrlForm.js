32da45e6d7b266918d811d0953d9eaadcf571793
ananya576104 <paletiananya@gmail.com>
2025-04-08 20:09:35 +0530

    Add files via upload

---
+++ frontend/src/components/UrlForm.js
@@ -0,0 +1,12 @@
+// src/components/UrlForm.js
+import React from 'react';
+
+const UrlForm = () => {
+  return (
+    <div>
+      <h2>URL Form Placeholder</h2>
+    </div>
+  );
+};
+
+export default UrlForm;
