407ec28b6555cc5402f1e2e3debd2907f5636e7a
ananya576104 <paletiananya@gmail.com>
2025-04-08 20:08:47 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UrlForm from "./components/UrlForm";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center p-4">
+      <UrlForm />
+    </div>
+  );
+}
+
+export default App;
